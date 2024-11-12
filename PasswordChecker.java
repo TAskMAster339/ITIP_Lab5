18c999342173336e334066445c85a79ba2f65103
Dima <dimagr3214@gmail.com>
2024-11-12 20:45:09 +0300

    Fix passwordCkecker

--- PasswordChecker.java
+++ PasswordChecker.java
@@ -7,9 +7,25 @@ public class PasswordChecker {
         try{
             Scanner scan = new Scanner(System.in);
             String password = scan.nextLine(); //Hello123 hello2134
-            Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*\\d)[A-Za-z0-9]{8,16}$");
-            Matcher matcher = pattern.matcher(password);
-            System.out.println(matcher.matches() ? "Password is strong" : "Password is weak");
+//            Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*\\d)[A-Za-z0-9]{8,16}$");
+//            Matcher matcher = pattern.matcher(password);
+//            System.out.println(matcher.matches() ? "Password is strong" : "Password is weak");
+            Pattern pattern1 = Pattern.compile(".*[A-Z].*");
+            Pattern pattern2 = Pattern.compile(".*[a-z].*");
+            Pattern pattern3 = Pattern.compile(".*[0-9].*");
+            if (password.length() > 16){
+                System.out.println("Слишком длинный пароль");
+            } else if (password.length() < 8){
+                System.out.println("Слишком короткий пароль");
+            } else if (!pattern1.matcher(password).matches()){
+                System.out.println("Нет большой буквы");
+            } else if (!pattern2.matcher(password).matches()){
+                System.out.println("Нет маленькой буквы");
+            } else if (!pattern3.matcher(password).matches()){
+                System.out.println("Нет цифры");
+            } else {
+                System.out.println("Пароль правильный");
+            }
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
